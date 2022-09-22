8c2a1102c8ff994d9fc664734100b4b37618d9a8
Michelle Austin <michelle.austin@itv.com>
2022-09-22 05:52:32 -0400

    first commit

---
+++ src/main/scala/Astar2.scala
@@ -0,0 +1,96 @@
+import scala.annotation.tailrec
+import scala.collection.mutable
+import scala.util.{Failure, Success, Try}
+
+object Astar2 extends App {
+
+  case class Coordinates(x: Int, y: Int)
+  case class Node(coordinates: Coordinates, parent: Option[Node], f: Double)
+
+  //TODO need an implicit for an ordering of Node -- not sure yet if this is really going to work
+  implicit val keyOrdering = new Ordering[Node] {
+    override def compare(x: Node, y: Node): Int =
+      x.toString.compareTo(y.toString)
+  }
+
+  val startNode = Node(Coordinates(0, 0), None, 0.0)
+  val goalNode  = Node(Coordinates(10, 10), None, 0.0) //TODO this shouldn't actually be a none, but just putting these here for now
+
+  val openList: mutable.PriorityQueue[Node] = mutable.PriorityQueue(startNode)
+  val closedList: List[Node]                = List.empty
+
+
+  @tailrec
+  def moveNodesFromOpenListToClosedList(
+    openList: mutable.PriorityQueue[Node],
+    closedList: List[Node]
+  ): (mutable.PriorityQueue[Node], List[Node]) =
+    tryToGetFirstElementInList(openList) match {
+      case Success(node) => moveNodesFromOpenListToClosedList(openList, closedList ++ List(node)) //TODO get neighhours and update open list
+      case Failure(e)    => (openList, closedList)
+    }
+
+  def tryToGetFirstElementInList(openList: mutable.PriorityQueue[Node]): Try[Node] = Try {
+    openList.dequeue
+  }
+
+  def nodeIsInOpenList(neighbour: Node, openList: mutable.PriorityQueue[Node]): Option[Node] = openList.find(_.coordinates == neighbour.coordinates)
+  def nodeIsInClosedList(neighbour: Node, closedList: List[Node]): Option[Node]              = closedList.find(_.coordinates == neighbour.coordinates)
+  def fIsSmallerThanListF(neighbour: Node, listNode: Node): Boolean =
+    if (neighbour.f < listNode.f) {
+      true
+    } else false
+
+  def addNodeToOpenList(neighbour: Node, openList: mutable.PriorityQueue[Node], closedList: List[Node]): Option[Node] =
+    for {
+      passedOpenListCheck   <- openListCheck(neighbour, openList)     //if this returns a None, we skip this neighbour and move on
+      passedClosedListCheck <- closedListCheck(neighbour, closedList) //if this returns a none, we skip this neighbour, if it returns a Some, we add that to the open list
+    } yield passedClosedListCheck
+
+  def openListCheck(neighbour: Node, openList: mutable.PriorityQueue[Node]): Option[Node] =
+    //find out if neighbour is in the open list
+    //if no, then return the node for further processing
+    //if yes, compare the f values
+    //if the node already in the open list has a lower f value, return a none
+    //if the node already in the open list has a higher f value, return the node to be added to the open list
+    nodeIsInOpenList(neighbour, openList) match {
+      case None => Some(neighbour)
+      case Some(existsInOpenList) =>
+        if (fIsSmallerThanListF(neighbour, existsInOpenList)) {
+          Some(neighbour)
+        } else None
+    }
+  def closedListCheck(neighbour: Node, closedList: List[Node]): Option[Node] =
+    //find out if neighbour is already in the closed list
+    //if no, add it to the open list
+    //if yes, find out if it has a lower f
+    //if yes skip,
+    //if no, add to the open list
+    nodeIsInClosedList(neighbour, closedList) match {
+      case None => Some(neighbour)
+      case Some(existsInClosedList) =>
+        if (fIsSmallerThanListF(neighbour, existsInClosedList)) {
+          Some(neighbour)
+        } else None
+    }
+
+  def getNeighbours(q: Node): List[Node] =
+    ???
+  //will need to calculate the fscore for each neighbour
+  //val fScore: Double = gScore(node) + hScore(node.coordinates, goal.coordinates)
+
+  def gScore(n: Node): Double =
+    ???
+  //TODO this adds together the distance between this node and q and also adds to it the g value for the start node
+
+//  def hScore(startCoordinates: Coordinates, endCoordinates: Coordinates) = {
+//    val xDist = (endCoordinates.x - startCoordinates.x).toDouble
+//    val yDist = (endCoordinates.y - startCoordinates.y).toDouble
+//    hypot(xDist, yDist)
+//  }
+
+//  def fScore = gScore + hScore
+
+  def findLowestF(neighbours: List[Node])              = ???
+  def compareAgainstClosedList(neighbours: List[Node]) = ???
+}
---
+++ src/test/scala/AStarTest.scala
@@ -0,0 +1,60 @@
+import Astar2.{Coordinates, Node, fIsSmallerThanListF}
+
+import scala.collection.mutable
+
+object AStarTest extends App {
+
+
+  implicit val keyOrdering = new Ordering[Node] {
+    override def compare(x: Node, y: Node): Int =
+      x.toString.compareTo(y.toString)
+  }
+
+  val neighbour1 = Node(Coordinates(5,4),None, 10)
+  val neighbour2 = Node(Coordinates(0,0),None, 10)
+  val neighbour3 = Node(Coordinates(0,1),None, 1)
+  val openList = mutable.PriorityQueue(neighbour1, neighbour2, neighbour3)
+
+  val firstNode = Node(Coordinates(5,4),None, 10)
+  val secondNode = Node(Coordinates(3,3),None, 1)
+  val closedList = List(firstNode, secondNode)
+
+
+  //OpenListCheck
+  val inOpenList = Node(Coordinates(0,0), None, 1)
+  val openListCheckResult = Astar2.openListCheck(inOpenList, openList)
+  println(openListCheckResult)
+
+  //ClosedListCheck
+  val isInClosedList = Node(Coordinates(5, 4), None, 1)
+  val closedListCheckResult = Astar2.closedListCheck(isInClosedList, closedList)
+  println(closedListCheckResult)
+
+  //Add to OpenList Check
+  val shouldBeAdded = Node(Coordinates(10, 10), None, 1) //not in either list
+  val addToOpenListCheck = Astar2.addNodeToOpenList(shouldBeAdded, openList, closedList)
+  println(addToOpenListCheck)
+
+  val shouldNotBeAdded = Node(Coordinates(5, 4), None, 10) //already in open List and doesn't have a better f
+  val addToOpenListFail = Astar2.addNodeToOpenList(shouldNotBeAdded, openList, closedList)
+  println(addToOpenListFail)
+
+  val shouldNotBeAdded2 = Node(Coordinates(3, 3), None, 10) //already in closed List and doesn't have a better f
+  val addToOpenListFail2 = Astar2.addNodeToOpenList(shouldNotBeAdded, openList, closedList)
+  println(addToOpenListFail2)
+
+
+  //Moving nodes from open to closed list
+  val goal = Node(Coordinates(5, 5), None, 1)
+  val openList3 = mutable.PriorityQueue(firstNode, secondNode)
+  val closedList3 = List(Node(Coordinates(0, 0), None, 1))
+  val moveNodes = Astar2.moveNodesFromOpenListToClosedList(openList3, closedList3)
+  println(moveNodes)
+
+  //Moving nodes from open to closed list when priority queue is emtpy
+  val goal2 = Node(Coordinates(5, 5), None, 1)
+  val openList4 = mutable.PriorityQueue()
+  val closedList4 = List(Node(Coordinates(0, 0), None, 1))
+  val moveNodes2 = Astar2.moveNodesFromOpenListToClosedList(openList4, closedList4)
+  println(moveNodes2)
+}
