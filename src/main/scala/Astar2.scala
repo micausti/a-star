30e9ea7e5ab9fa3ec678452ac059580208eeceac
Michelle Austin <michelle.austin@itv.com>
2022-09-30 08:52:40 -0600

    neighbour processing (one failing test)

--- src/main/scala/Astar2.scala
+++ src/main/scala/Astar2.scala
@@ -6,7 +6,7 @@ import scala.util.{Failure, Success, Try}
 object Astar2 extends App {
 
   case class Coordinates(x: Int, y: Int)
-  case class Node(coordinates: Coordinates, parent: Option[Node], f: Double, h: Double, g: Double)
+  case class Node(coordinates: Coordinates, parent: Option[Node], h: Double, g: Double)
 
   //TODO need an implicit for an ordering of Node -- not sure yet if this is really going to work
   implicit val keyOrdering = new Ordering[Node] {
@@ -14,8 +14,8 @@ object Astar2 extends App {
       x.toString.compareTo(y.toString)
   }
 
-  val startNode                             = Node(Coordinates(0, 0), None, 0.0, 0.0, 0.0)
-  val goalNode                              = Node(Coordinates(10, 10), None, 0.0, 0.0, 0.0) //TODO this shouldn't actually be a none, but just putting these here for now
+  val startNode                             = Node(Coordinates(0, 0), None, 0.0, 0.0)
+  val goalNode                              = Node(Coordinates(10, 10), None, 0.0, 0.0)
   val openList: mutable.PriorityQueue[Node] = mutable.PriorityQueue(startNode)
   val closedList: List[Node]                = List.empty
 
@@ -31,7 +31,7 @@ object Astar2 extends App {
       case Failure(err)  => ???
     }
 
-  @tailrec //TODO need to get the list of neighbours to pass in here
+  @tailrec
   def moveNodesToClosedList(
     goal: Node,
     openList: mutable.PriorityQueue[Node],
@@ -44,24 +44,32 @@ object Astar2 extends App {
         if (node.coordinates == goal.coordinates) {
           moveNodesToClosedList(goal, openList, closedList ++ List(node)) //Do we need to stop all processing if we get to this point or is it ok to keep going?
         } else {
-          processNeighbours(node, openList, closedList)
+          val neighbours: List[Node] = getNeighbours(node, goal)
+          processNeighbours(neighbours, openList, closedList)
         }
       case Failure(err) =>
         (openList, closedList) //If we can't get anything off the open list to process, then we will return the closed list and be finished
     }
 
-  //TODO need to change this so we pass in the list of neighbours
-  def processNeighbours(neighbour: Node, openList: mutable.PriorityQueue[Node], closedList: List[Node]): (mutable.PriorityQueue[Node], List[Node]) =
-    if (shouldAddNodeToOpenList(neighbour, openList, closedList)) {
-      (openList.addOne(neighbour), closedList)
-    } else {
-      (openList, closedList)
+  @tailrec
+  def processNeighbours(
+    neighbours: List[Node],
+    openList: mutable.PriorityQueue[Node],
+    closedList: List[Node]
+  ): (mutable.PriorityQueue[Node], List[Node]) =
+    neighbours match {
+      case Nil => (openList, closedList)
+      case x :: xs =>
+        if (shouldAddNodeToOpenList(x, openList, closedList)) {
+          (openList.addOne(x), closedList)
+        }
+        processNeighbours(xs, openList, closedList)
     }
 
   def nodeIsInOpenList(neighbour: Node, openList: mutable.PriorityQueue[Node]): Option[Node] = openList.find(_.coordinates == neighbour.coordinates)
   def nodeIsInClosedList(neighbour: Node, closedList: List[Node]): Option[Node]              = closedList.find(_.coordinates == neighbour.coordinates)
   def fIsSmallerThanListF(neighbour: Node, listNode: Node): Boolean =
-    if (neighbour.f < listNode.f) {
+    if (f(neighbour) < f(listNode)) {
       true
     } else false
 
@@ -94,29 +102,33 @@ object Astar2 extends App {
     }
 
   //In this implementation we are going to get the 8 surrounding squares
-  //TODO need to set the parent, but where should this happen?
-  def getNeighbours(q: Node): List[Node] = {
-    val node1 = Node(Coordinates(q.coordinates.x - 1, q.coordinates.y - 1), None, 0.0, 0.0, 0.0)
-    val node2 = Node(Coordinates(q.coordinates.x, q.coordinates.y - 1), None, 0.0, 0.0, 0.0)
-    val node3 = Node(Coordinates(q.coordinates.x + 1, q.coordinates.y - 1), None, 0.0, 0.0, 0.0)
-    val node4 = Node(Coordinates(q.coordinates.x - 1, q.coordinates.y), None, 0.0, 0.0, 0.0)
-    val node5 = Node(Coordinates(q.coordinates.x + 1, q.coordinates.y), None, 0.0, 0.0, 0.0)
-    val node6 = Node(Coordinates(q.coordinates.x - 1, q.coordinates.y + 1), None, 0.0, 0.0, 0.0)
-    val node7 = Node(Coordinates(q.coordinates.x, q.coordinates.y + 1), None, 0.0, 0.0, 0.0)
-    val node8 = Node(Coordinates(q.coordinates.x + 1, q.coordinates.y + 1), None, 0.0, 0.0, 0.0)
+  def getNeighbours(q: Node, goal: Node): List[Node] = {
+    val node1       = Node(Coordinates(q.coordinates.x - 1, q.coordinates.y - 1), Some(q), 0.0, 0.0)
+    val node2       = Node(Coordinates(q.coordinates.x, q.coordinates.y - 1), Some(q), 0.0, 0.0)
+    val node3       = Node(Coordinates(q.coordinates.x + 1, q.coordinates.y - 1), Some(q), 0.0, 0.0)
+    val node4       = Node(Coordinates(q.coordinates.x - 1, q.coordinates.y), Some(q), 0.0, 0.0)
+    val node5       = Node(Coordinates(q.coordinates.x + 1, q.coordinates.y), Some(q), 0.0, 0.0)
+    val node6       = Node(Coordinates(q.coordinates.x - 1, q.coordinates.y + 1), Some(q), 0.0, 0.0)
+    val node7       = Node(Coordinates(q.coordinates.x, q.coordinates.y + 1), Some(q), 0.0, 0.0)
+    val node8       = Node(Coordinates(q.coordinates.x + 1, q.coordinates.y + 1), Some(q), 0.0, 0.0)
     val initialList = List(node1, node2, node3, node4, node5, node6, node7, node8)
-    initialList.map(n => n.copy(h = hScore(q, n))) //gets neighbours and calculates the hscore
+    initialList.map(n => n.copy(h = h(n, goal), g = g(q, 0.0))) //gets neighbours and calculates the hscore
   }
 
-  def hScore(q: Node, n: Node): Double = { //using the euclidian distance for the heuristic
-    Math.hypot(abs(q.coordinates.x - n.coordinates.x), abs(q.coordinates.y - n.coordinates.y))
-  }
+  //the estimated movement cost to move from that given square on the grid to the final destination
+  def h(n: Node, goal: Node): Double =
+    Math.hypot(abs(n.coordinates.x - goal.coordinates.x), abs(n.coordinates.y - goal.coordinates.y))
 
-  def gScore = ??? //TODO the movement cost to move from the starting point to a given square on the grid, following the path generated to get there.
+  //the movement cost to move from the starting point to a given square on the grid, following the path generated to get there
+  @tailrec
+  def g(q: Node, accDistance: Double): Double =
+    q.parent match {
+      case Some(n) =>
+        val distance = Math.hypot(abs(q.coordinates.x - n.coordinates.x), abs(q.coordinates.y - n.coordinates.y))
+        g(n, distance + accDistance)
+      case None => accDistance
+    }
 
+  def f(node: Node) = node.g + node.h
 
-//  def fScore = gScore + hScore
-
-  def findLowestF(neighbours: List[Node])              = ???
-  def compareAgainstClosedList(neighbours: List[Node]) = ???
 }
--- src/test/scala/AStarTest.scala
+++ src/test/scala/AStarTest.scala
@@ -66,17 +66,86 @@ class AStarTest extends munit.FunSuite {
   shouldAddNodeTest("should return true if it passes both checks", existsInClosedListWithSmallerF, openList, closedList, true)
 
   test("make sure we get the right neighbours") {
+    val node = Node(Coordinates(2, 2), None, 0.0, 0.0)
+    val goal = Node(Coordinates(2, 2), None, 0.0, 0.0)
     val expected = List(
-      Node(Coordinates(1, 1), None, 0.0, 0.0, 0.0),
-      Node(Coordinates(2, 1), None, 0.0, 0.0, 0.0),
-      Node(Coordinates(3, 1), None, 0.0, 0.0, 0.0),
-      Node(Coordinates(1, 2), None, 0.0, 0.0, 0.0),
-      Node(Coordinates(3, 2), None, 0.0, 0.0, 0.0),
-      Node(Coordinates(1, 3), None, 0.0, 0.0, 0.0),
-      Node(Coordinates(2, 3), None, 0.0, 0.0, 0.0),
-      Node(Coordinates(3, 3), None, 0.0, 0.0, 0.0)
+      Node(Coordinates(1, 1), None, 1.4142135623730951, 0.0),
+      Node(Coordinates(2, 1), None, 1.0, 0.0),
+      Node(Coordinates(3, 1), None, 1.4142135623730951, 0.0),
+      Node(Coordinates(1, 2), None, 1.0, 0.0),
+      Node(Coordinates(3, 2), None, 1.0, 0.0),
+      Node(Coordinates(1, 3), None, 1.4142135623730951, 0.0),
+      Node(Coordinates(2, 3), None, 1.0, 0.0),
+      Node(Coordinates(3, 3), None, 1.4142135623730951, 0.0)
     )
-    assertEquals(Astar2.getNeighbours(Node(Coordinates(2, 2), None, 0.0, 0.0, 0.0)), expected)
+    assertEquals(Astar2.getNeighbours(node, goal), expected)
+  }
+
+  test("make sure we calculate g and h correctly for neighbours") {
+    val firstNode  = Node(Coordinates(0, 0), None, 0.0, 0.0)
+    val secondNode = Node(Coordinates(3, 3), Some(firstNode), 0.0, 0.0)
+    val thirdNode  = Node(Coordinates(5, 5), Some(secondNode), 0.0, 0.0)
+    val goalNode   = Node(Coordinates(6, 6), None, 0.0, 0.0)
+    val expected = List(
+      Node(Coordinates(4, 4), None, 2.8284271247461903, 7.0710678118654755),
+      Node(Coordinates(5, 4), None, 2.23606797749979, 7.0710678118654755),
+      Node(Coordinates(6, 4), None, 2.0, 7.0710678118654755),
+      Node(Coordinates(4, 5), None, 2.23606797749979, 7.0710678118654755),
+      Node(Coordinates(6, 5), None, 1.0, 7.0710678118654755),
+      Node(Coordinates(4, 6), None, 2.0, 7.0710678118654755),
+      Node(Coordinates(5, 6), None, 1.0, 7.0710678118654755),
+      Node(Coordinates(6, 6), None, 0.0, 7.0710678118654755)
+    )
+    assertEquals(Astar2.getNeighbours(thirdNode, goalNode), expected)
+  }
+
+  test("calculate gscore correctly when there are multiple parent nodes") {
+    val firstNode  = Node(Coordinates(0, 0), None, 0.0, 0.0)
+    val secondNode = Node(Coordinates(3, 3), Some(firstNode), 0.0, 0.0)
+    val thirdNode  = Node(Coordinates(5, 5), Some(secondNode), 0.0, 0.0)
+    val gscore     = Astar2.g(thirdNode, 0.0)
+    val expected   = 7.0710678118654755
+    assertEquals(gscore, expected)
+  }
+
+  test("calculate gscore correctly when there is no parent node") {
+    val firstNode = Node(Coordinates(0, 0), None, 0.0, 0.0)
+    val gscore    = Astar2.g(firstNode, 0.0)
+    val expected  = 0.0
+    assertEquals(gscore, expected)
+  }
+
+  //TODO issue with this test
+  test("process neighbours should correctly add nodes to the open  list") {
+    val startNode = Node(Coordinates(5, 5), None, 0.0, 0.0)
+    val listOfNeighbours = List(
+      Node(Coordinates(4, 4), None, 2.8284271247461903, 7.0710678118654755),
+      Node(Coordinates(5, 4), None, 2.23606797749979, 7.0710678118654755),
+      Node(Coordinates(6, 4), None, 2.0, 7.0710678118654755),
+      Node(Coordinates(4, 5), None, 2.23606797749979, 7.0710678118654755),
+      Node(Coordinates(6, 5), None, 1.0, 7.0710678118654755),
+      Node(Coordinates(4, 6), None, 2.0, 7.0710678118654755),
+      Node(Coordinates(5, 6), None, 1.0, 7.0710678118654755),
+      Node(Coordinates(6, 6), None, 0.0, 7.0710678118654755)
+    )
+    val openList   = mutable.PriorityQueue(startNode)
+    val closedList = List.empty
+    val process    = Astar2.processNeighbours(listOfNeighbours, openList, closedList)
+    val expected = (
+      mutable.PriorityQueue(
+        Node(Coordinates(6, 6), None, 0.0, 7.0710678118654755),
+        Node(Coordinates(6, 5), None, 1.0, 7.0710678118654755),
+        Node(Coordinates(6, 4), None, 2.0, 7.0710678118654755),
+        Node(Coordinates(5, 6), None, 1.0, 7.0710678118654755),
+        Node(Coordinates(4, 5), None, 2.23606797749979, 7.0710678118654755),
+        Node(Coordinates(5, 4), None, 2.23606797749979, 7.0710678118654755),
+        Node(Coordinates(4, 6), None, 2.0, 7.0710678118654755),
+        Node(Coordinates(4, 4), None, 2.8284271247461903, 7.0710678118654755),
+        Node(Coordinates(5, 5), None, 0.0, 0.0)
+      ),
+      List()
+    )
+    assertEquals(process, expected)
   }
 }
 
@@ -86,20 +155,20 @@ object Tests {
       x.toString.compareTo(y.toString)
   }
 
-  val neighbour1 = Node(Coordinates(5, 4), None, 10, 0.0, 0.0)
-  val neighbour2 = Node(Coordinates(0, 0), None, 10, 0.0, 0.0)
-  val neighbour3 = Node(Coordinates(0, 1), None, 1, 0.0, 0.0)
+  val neighbour1 = Node(Coordinates(5, 4), None, 10.0, 0.0)
+  val neighbour2 = Node(Coordinates(0, 0), None, 10.0, 0.0)
+  val neighbour3 = Node(Coordinates(0, 1), None, 1.0, 0.0)
   val openList   = mutable.PriorityQueue(neighbour1, neighbour2, neighbour3)
 
-  val firstNode  = Node(Coordinates(5, 4), None, 10, 0.0, 0.0)
-  val secondNode = Node(Coordinates(3, 3), None, 1, 0.0, 0.0)
+  val firstNode  = Node(Coordinates(5, 4), None, 10.0, 0.0)
+  val secondNode = Node(Coordinates(3, 3), None, 1.0, 0.0)
   val closedList = List(firstNode, secondNode)
 
-  val existsInOpenListWithSmallerF = Node(Coordinates(0, 0), None, 1, 0.0, 0.0)
-  val doesNotExistInOpenList       = Node(Coordinates(3, 3), None, 1, 0.0, 0.0)
-  val existsInOpenListWithLargerF  = Node(Coordinates(0, 0), None, 11, 0.0, 0.0)
+  val existsInOpenListWithSmallerF = Node(Coordinates(0, 0), None, 1.0, 0.0)
+  val doesNotExistInOpenList       = Node(Coordinates(3, 3), None, 1.0, 0.0)
+  val existsInOpenListWithLargerF  = Node(Coordinates(0, 0), None, 11.0, 0.0)
 
-  val existsInClosedListWithSmallerF = Node(Coordinates(0, 0), None, 1, 0.0, 0.0)
-  val doesNotExistInClosedList       = Node(Coordinates(3, 4), None, 1, 0.0, 0.0)
-  val existsInClosedListWithLargerF  = Node(Coordinates(3, 3), None, 11, 0.0, 0.0)
+  val existsInClosedListWithSmallerF = Node(Coordinates(0, 0), None, 1.0, 0.0)
+  val doesNotExistInClosedList       = Node(Coordinates(3, 4), None, 1.0, 0.0)
+  val existsInClosedListWithLargerF  = Node(Coordinates(3, 3), None, 11.0, 0.0)
 }
